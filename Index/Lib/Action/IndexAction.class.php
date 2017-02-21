d81ddc9c96bc4e7ee5b97cbcb4389d57a17ca6e6
Xiaosea <maysrp@gmail.com>
2017-02-22 05:21:12 +0800

    首页轮播

--- Index/Lib/Action/IndexAction.class.php
+++ Index/Lib/Action/IndexAction.class.php
@@ -2,7 +2,9 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-		$this->display();
+	$info=D('Video')->ten();
+        $this->assign('info',$info);
+	$this->display();
     }
     public function video(){
     	$vid=$this->_get('vid');
